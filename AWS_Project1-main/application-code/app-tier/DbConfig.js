dab095fd249457149448d56348278ac38398a835
Cloud-Lucky <laxmireddy870@gmail.com>
2025-11-04 17:56:15 +0530

    first commit

---
+++ AWS_Project1-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : "database-1.c2bw42mqojsc.us-east-1.rds.amazonaws.com",
+    DB_USER : "admin",
+    DB_PWD : "Garuda123",
+    DB_DATABASE : "webappdb"
+});
